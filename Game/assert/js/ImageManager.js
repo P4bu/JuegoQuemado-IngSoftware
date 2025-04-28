aa250f56a8295de202831c763c81b97201763ba7
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2025-04-27 22:28:31 -0400

    Se modifican las imagenes y se trabaja en el flujo

--- Game/assert/js/ImageManager.js
+++ Game/assert/js/ImageManager.js
@@ -1,17 +1,28 @@
+const IMAGE_1_VIDA = "https://piskel-imgstore-b.appspot.com/img/526672b8-23d4-11f0-b4e1-2fdfd4e47724.gif";
+const IMAGE_2_VIDAS = "https://piskel-imgstore-b.appspot.com/img/de1e8702-23d7-11f0-824c-2fdfd4e47724.gif";
+const IMAGE_3_VIDAS = "https://piskel-imgstore-b.appspot.com/img/b9644c9c-23d7-11f0-b734-2fdfd4e47724.gif";
+const IMAGE_5_VIDAS = "https://piskel-imgstore-b.appspot.com/img/e5772245-23d4-11f0-8940-2fdfd4e47724.gif";
+const IMAGE_4_VIDAS = "https://piskel-imgstore-b.appspot.com/img/3c8ca20c-23d7-11f0-ab91-2fdfd4e47724.gif"; // 2 piernas
+const IMAGE_6_VIDAS  = "https://piskel-imgstore-b.appspot.com/img/ffc3c5b3-23d4-11f0-bd65-2fdfd4e47724.gif";
+const IMAGE_7_VIDAS = "https://piskel-imgstore-b.appspot.com/img/e63c4fcf-23d6-11f0-b8f9-2fdfd4e47724.gif";
+const IMAGE_GAME_OVER = "https://piskel-imgstore-b.appspot.com/img/526672b8-23d4-11f0-b4e1-2fdfd4e47724.gif";
+
 export default class ImageManager {
+
     constructor() {
         this.currentAttempts = 6;
         this.maxAttempts = 6;
         this.imageElement = null;
         this.attemptsElement = null;
         this.imagePaths = [
-            "/api/placeholder/200/250?text=6_vidas",
-            "/api/placeholder/200/250?text=5_vidas",
-            "/api/placeholder/200/250?text=4_vidas",
-            "/api/placeholder/200/250?text=3_vidas",
-            "/api/placeholder/200/250?text=2_vidas",
-            "/api/placeholder/200/250?text=1_vida",
-            "/api/placeholder/200/250?text=Game_Over"
+            IMAGE_7_VIDAS,
+            IMAGE_6_VIDAS,
+            IMAGE_5_VIDAS,
+            IMAGE_4_VIDAS,
+            IMAGE_3_VIDAS,
+            IMAGE_2_VIDAS,
+            IMAGE_1_VIDA,
+            IMAGE_GAME_OVER
         ];
     }
 
